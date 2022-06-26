507f3a1cda23b14cd05c1609b0acc497eef1e9cf
Tianea2160 <rhlehfndvkd7557@daum.net>
2022-06-26 13:45:06 +0900

    setting gitignore for removing ds_store and add navbar input

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,20 @@
 import {useState} from "react";
 
 const Navbar = () => {
-    const [search, setSearch] = useState("")
+    const [search, setSearch] = useState("");
 
     return (
         <div className="Navbar">
             <div className="NavInfo">
                 <span>Nav</span>
-                <input type="text"/>
+                <input
+                    value={search}
+                    onChange={(e) => {
+                        setSearch(e.target.value)
+                    }}
+                    type="text"
+                />
             </div>
         </div>)
 }
-
 export default Navbar;
